b3c15b8bc2b4ac537df562a3ad7777c0e922483a
Scott Chang <scott68.chang@advantech.com.tw>
2017-08-16 16:35:07 +0800

    [EI-Connect] 1. add batch file to manage subtree

---
+++ subtree.bat
@@ -0,0 +1,21 @@
+set cmd=push
+set branch=%2
+IF "%1" == "push" (
+	set cmd=%1
+)
+IF "%1" == "add" (
+	set cmd=%1
+)
+IF "%1" == "pull" (
+	set cmd=%1
+)
+IF "%2" == "" (
+	set branch=master
+)
+echo "%cmd%"
+
+git subtree %cmd% --prefix=Library/MosquittoCarrier https://github.com/ADVANTECH-Corp/WISECarrier-Mosquitto.git %branch%
+git subtree %cmd% --prefix=Library/MQTTConnector https://github.com/ADVANTECH-Corp/WISEConnect-MQTT.git %branch%
+git subtree %cmd% --prefix=Library/WISECore https://github.com/ADVANTECH-Corp/WISECore.git %branch%
+git subtree %cmd% --prefix=Platform https://github.com/ADVANTECH-Corp/EI-Platform.git %branch%
+git subtree %cmd% --prefix=Include\wise https://github.com/ADVANTECH-Corp/WISEPredefine.git %branch%
