f2bbd18016aab032a6bc6aa53f63d9ac16519943
Scott Chang <scott68.chang@advantech.com.tw>
2017-08-16 17:05:31 +0800

    [EI-Connect] 1. update subtree batch file

--- subtree.bat
+++ subtree.bat
@@ -1,3 +1,4 @@
+@echo off
 set cmd=push
 set branch=%2
 IF "%1" == "push" (
@@ -12,10 +13,18 @@ IF "%1" == "pull" (
 IF "%2" == "" (
 	set branch=master
 )
-echo "%cmd%"
 
+echo git subtree %cmd% --prefix=Library/MosquittoCarrier https://github.com/ADVANTECH-Corp/WISECarrier-Mosquitto.git %branch%
 git subtree %cmd% --prefix=Library/MosquittoCarrier https://github.com/ADVANTECH-Corp/WISECarrier-Mosquitto.git %branch%
+
+echo git subtree %cmd% --prefix=Library/MQTTConnector https://github.com/ADVANTECH-Corp/WISEConnect-MQTT.git %branch%
 git subtree %cmd% --prefix=Library/MQTTConnector https://github.com/ADVANTECH-Corp/WISEConnect-MQTT.git %branch%
+
+echo git subtree %cmd% --prefix=Library/WISECore https://github.com/ADVANTECH-Corp/WISECore.git %branch%
 git subtree %cmd% --prefix=Library/WISECore https://github.com/ADVANTECH-Corp/WISECore.git %branch%
+
+echo git subtree %cmd% --prefix=Platform https://github.com/ADVANTECH-Corp/EI-Platform.git %branch%
 git subtree %cmd% --prefix=Platform https://github.com/ADVANTECH-Corp/EI-Platform.git %branch%
+
+echo git subtree %cmd% --prefix=Include\wise https://github.com/ADVANTECH-Corp/WISEPredefine.git %branch%
 git subtree %cmd% --prefix=Include\wise https://github.com/ADVANTECH-Corp/WISEPredefine.git %branch%
